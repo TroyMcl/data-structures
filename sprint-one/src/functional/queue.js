d915d8f0ecdd7061966c5d05d1e8ae4a58f47375
Troy <troyjmcl85@gmail.com>
2019-06-10 21:32:41 -0600

    all tests and pomander passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,23 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
   };
 
   someInstance.dequeue = function() {
+    count--;
   };
 
   someInstance.size = function() {
+    if (count < 0) {
+      return 0;
+    }
+    return count;
   };
 
   return someInstance;
